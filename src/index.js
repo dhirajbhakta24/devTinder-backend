bad001b5cbf577fed5c61f33d689dcc314825035
Dhiraj Kumar Bhakta <bhaktadhiraj@gmail.com>
2024-10-03 22:53:22 +0530

    update and delete apis

--- src/index.js
+++ src/index.js
@@ -16,6 +16,50 @@ app.post('/signup',async (req,res)=>{
         res.status(400).send("error saving the user" + err.message);
     }
 });
+
+//get user by emailId
+app.get('/feed', async (req,res) =>{
+    const userEmail =   req.body.emailId;
+
+    try {
+        const user =  await User.findOne({emailId : userEmail});
+        if(!user){
+            res.status(404).send("User not found");
+        }
+        else{
+            res.send(user);
+        }
+    } catch (error) {
+        res.status(400).send("Something went wrong ");
+    }
+
+});
+// Detele a user from the database
+app.delete("/user", async (req, res) => {
+    const userId = req.body.userId;
+    try {
+      const user = await User.findByIdAndDelete({ _id: userId });
+      //const user = await User.findByIdAndDelete(userId);
+      res.send("User deleted successfully");
+    } catch (err) {
+      res.status(400).send("Something went wrong ");
+    }
+  });
+  // Update data of the user
+  app.patch("/user", async (req, res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+    try {
+      const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+        returnDocument: "after",
+      });
+      console.log(user);
+      res.send("User updated successfully");
+    } catch (err) {
+      res.status(400).send("Something went wrong ");
+    }
+  });
+
 PORT = 7777;
 
 connectDB()
